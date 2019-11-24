f55de122b5fca4bbb533d3f836d300bc5db95539
Alias M6 <aliasmontoya719@gmail.com>
2019-11-24 10:00:45 -0700

    make use of redux combineReducers function to wire up reducers to work with the rest of the app

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const songsReducer = () => {
   return [
     { title: 'No Scrubs', duration: '4:05' },
@@ -12,5 +14,10 @@ const selectedSongReducer = (selectedSong = null, action) => {
   if (action.type === 'SONG_SELECTED') {
     return action.payload;
   }
-  return selectedSong; 
+  return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
