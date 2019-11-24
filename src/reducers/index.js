a31f9862d03fd165f9a4492eedad036ea7dd823b
Alias M6 <aliasmontoya719@gmail.com>
2019-11-24 09:57:01 -0700

    create reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Smells Like Teen Spirit',
+    duration: '3:30' },
+    { title: 'Barbie Girl', duration: '3:05' },
+    { title: 'No Diggity', duration: '3:45' }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+  return selectedSong; 
+};
